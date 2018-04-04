4deac0d166db8c3334de8f212a798073ed24e8ef
bryan717 <barxroe@gmail.com>
2018-04-04 16:40:09 +0800

    now can query without z axis

--- connect.php
+++ connect.php
@@ -53,34 +53,20 @@ if ($conn->connect_error) {
 	$xAxis = $_POST['xAxis'];
 	$yAxis = $_POST['yAxis'];
 	$zAxis = $_POST['zAxis'];
-	if($xAxis=="not selected" || $yAxis=="not selected" || $zAxis=="not selected")
+	if($xAxis=="null"|| $yAxis=="null")
 		die("axis not selected");
-	if (isset($_POST['xAxis']) && $xAxis!="not selected")			
-		echo "You chose the following x-axis: " . $xAxis . "<br>";	
-	if (isset($_POST['yAxis']) && $yAxis!="not selected")			
+	else{	
+		echo "You chose the following x-axis: " . $xAxis . "<br>";				
 		echo "You chose the following y-axis: " . $yAxis . "<br>";
-	if (isset($_POST['zAxis']) && $zAxis!="not selected")			
+	}
+	if ($zAxis!="null")			
 		echo "You chose the following z-axis: " . $zAxis . "<br>";
 	
 	//choosing table based on axis category
 	$xCat = $_POST['xCat'];
 	$yCat = $_POST['yCat'];
 	$zCat = $_POST['zCat'];
-	
-	if($xCat == $yCat)
-	{
-		if($xCat == $zCat)
-			$cat = array($xCat);
-		else
-			$cat = array($xCat,$zCat);
-	}
-	else if($xCat == $zCat || $yCat == $zCat)
-		$cat = array($xCat,$yCat);
-	else
-		$cat = array($xCat,$yCat,$zCat);
-	
-	echo "axis category: " . implode(", ", $cat)."<br><br>";
-	
+		
 	//query portion of checkboxes, to integrate with axis query part
     if(count($climatearray))
 	{
@@ -100,10 +86,14 @@ if ($conn->connect_error) {
 
 	//sql query based on checkbox	
 	$query = "SELECT tax.cropID, tax.common_name, "
-			. $xCat . "." . $xAxis . ", " . $yCat . "." . $yAxis . ", " . $zCat . "." . $zAxis
-			. " FROM crop_taxonomy tax LEFT JOIN agro_agroecology_livedb agro ON tax.cropID=agro.cropid";	
+			. $xCat . "." . $xAxis . ", " . $yCat . "." . $yAxis;
+	if($zAxis!="null")
+		$query .= ", " . $zCat . "." . $zAxis;
+	$query .= " FROM crop_taxonomy tax LEFT JOIN agro_agroecology_livedb agro ON tax.cropID=agro.cropid";	
 	$query .= " LEFT JOIN nutrient_minerals mineral ON tax.cropID=mineral.cropid LEFT JOIN general_plant_parts part ON mineral.plant_part_id=part.id LEFT JOIN nutrient_proximate_composition composition ON tax.cropID=composition.cropid";
-	$query .=" WHERE " . $xAxis . " !=' ' AND " . $yAxis . " !=' ' AND " . $zAxis . " !=' '";
+	$query .=" WHERE " . $xAxis . " !=' ' AND " . $yAxis . " !=' '";
+	if($zAxis!="null")
+		$query .= " AND " . $zAxis . " !=' '";
 	if(count($partarray))
 		$query .= $partquery;
 	if(count($climatearray))
