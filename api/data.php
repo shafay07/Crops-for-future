8f899b34ed86b1e265ac6785af38559ea64c981f
Lowprophyle <khamanconsolidate@gmail.com>
2018-04-07 13:12:18 +0800

    filling individual but crop usage not finish yet

--- api/data.php
+++ api/data.php
@@ -5,18 +5,30 @@ header('Content-Type: application/json');
 // get the connection from conn.php
 require_once('../conn.php');
 
+// for datacompare
 if(isset($_POST['cropA'], $_POST['cropB'])){
 	$cropA = $_POST['cropA'];
 	$cropB = $_POST['cropB'];
+
+	$query = "SELECT * FROM `crop_taxonomy` 
+	LEFT JOIN `agro_agroecology_livedb` ON `agro_agroecology_livedb`.`cropid` = `crop_taxonomy`.`cropID`
+	LEFT JOIN `nutrient_minerals` ON `nutrient_minerals`.`cropid` = `crop_taxonomy`.`cropID`
+	LEFT JOIN `nutrient_proximate_composition` ON `nutrient_proximate_composition`.`cropid` = `crop_taxonomy`.`cropID`
+	WHERE `crop_taxonomy`.`name_var_lndrce` IN ('$cropA','$cropB');";
 }
 
-//query to get data from the table 
-// query for agro
-$query = "SELECT * FROM `crop_taxonomy` 
-LEFT JOIN `agro_agroecology_livedb` ON `agro_agroecology_livedb`.`cropid` = `crop_taxonomy`.`cropID`
-LEFT JOIN `nutrient_minerals` ON `nutrient_minerals`.`cropid` = `crop_taxonomy`.`cropID`
-LEFT JOIN `nutrient_proximate_composition` ON `nutrient_proximate_composition`.`cropid` = `crop_taxonomy`.`cropID`
-WHERE `crop_taxonomy`.`name_var_lndrce` IN ('$cropA','$cropB');";
+// for individual data
+if(isset($_POST['crop'])){
+	$crop = $_POST['crop'];
+
+	$query = "SELECT * FROM `crop_taxonomy` 
+	LEFT JOIN `agro_agroecology_livedb` ON `agro_agroecology_livedb`.`cropid` = `crop_taxonomy`.`cropID`
+	LEFT JOIN `nutrient_minerals` ON `nutrient_minerals`.`cropid` = `crop_taxonomy`.`cropID`
+	LEFT JOIN `nutrient_proximate_composition` ON `nutrient_proximate_composition`.`cropid` = `crop_taxonomy`.`cropID`
+	WHERE `crop_taxonomy`.`name_var_lndrce` = '$crop';";
+}
+
+
 
 //execute query
 $result = $mysqli->query($query);
